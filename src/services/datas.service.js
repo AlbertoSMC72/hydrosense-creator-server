cf610c0a4e01373e1258c7c7952090017b58c64b
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2024-07-02 22:51:07 -0600

    feat: update data service to use request body in postDataService

--- src/services/datas.service.js
+++ src/services/datas.service.js
@@ -3,7 +3,7 @@ import { createData } from "../repositories/datas.repositories";
 export const postDataService = async (req) => {
     try {
         //logica para el servidor en tiempo real
-        return await createData(req);
+        return await createData(req.body);
     } catch (error) {
         throw error;
     }
