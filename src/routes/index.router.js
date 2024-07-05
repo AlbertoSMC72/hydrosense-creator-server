d4d76ea50b98dfdbcf87748237540c59ae7b4e1f
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2024-07-05 00:52:34 -0600

    chore: Remove engine related code and files

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import datasRouter from "./datas.router.js";
 import reportRouter from "./reports.router.js";
-import engineRouter from "./engines.router.js";
 
 const router = Router();
 const prefijo = "app";
 
 router.use(`/${prefijo}/data`, datasRouter)
 router.use(`/${prefijo}/report`, reportRouter)
-router.use(`/${prefijo}/engine`, engineRouter)
+
 
 export default router;
