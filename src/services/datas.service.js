bfb2a0014430427cfaee0def86d77bdc6ecefa62
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2024-07-21 20:55:19 -0600

    chore: Update npm dependencies and add ws package for WebSocket communication

--- src/services/datas.service.js
+++ src/services/datas.service.js
@@ -1,17 +1,30 @@
-import { createData} from "../repositories/datas.repositories.js";
+import { createData } from "../repositories/datas.repositories.js";
 import { validateData } from "../models/datas.model.js";
+import WebSocket from 'ws';
 
 export const postDataService = async (req) => {
     try {
-        //logica para el servidor en tiempo real
         const data = validateData(req.body);
-        if(!data.success){
+        if (!data.success) {
+            // Crear un WebSocket client y enviar infoToSend
+            const ws = new WebSocket('ws://localhost:8080');
+            
+
+            ws.on('open', function open() {
+                ws.send(JSON.stringify(req.body));
+            });
+
+            ws.on('error', function error(err) {
+                console.error('WebSocket error:', err);
+            });
+
+            // Lógica para enviar a la DB
             const dataRes = await createData(req.body);
             return dataRes;
-        }else {
-            throw new Error(data.error.message)
+        } else {
+            throw new Error(data.error.message);
         }
     } catch (error) {
         throw error;
     }
-}
+};
