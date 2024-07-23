f2ba9bcd4deb31d76209b2e7146aa6a47e8231a5
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2024-07-23 05:30:15 -0600

    chore: Add socket.io-client package for Socket.IO communication and enable HTTPS server

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import index from './src/routes/index.router.js';
 import config from './src/config/config.js';
+import https from 'https';
+import fs from 'fs';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,10 +21,15 @@ app.use((req, res) => {
     res.status(404).send("Wrong route");
 });
 
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/hydrosense-info.integrador.xyz/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/hydrosense-info.integrador.xyz/fullchain.pem')
+};
+
 config.connect().then(() => {
     console.log("Database connected");
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
+    https.createServer(options, app).listen(PORT, () => {
+        console.log(`HTTPS Server running on port ${PORT}`);
     });
 }).catch((error) => {
     console.log("Error al iniciar el servidor:", error);
--- src/services/datas.service.js
+++ src/services/datas.service.js
@@ -1,25 +1,28 @@
 import { createData } from "../repositories/datas.repositories.js";
 import { validateData } from "../models/datas.model.js";
-import WebSocket from 'ws';
+import { io } from 'socket.io-client';
 
 export const postDataService = async (req) => {
     try {
         const data = validateData(req.body);
         if (!data.success) {
-            // Crear un WebSocket client y enviar infoToSend
-            const ws = new WebSocket('ws://localhost:8080');
+            // Crear un cliente de Socket.IO y enviar infoToSend
+            const socket = io('http://44.199.91.75:8080');
+
+            socket.on('connect', () => {
+                socket.emit('message', req.body);
+            });
+
+            socket.on('error', (err) => {
+                console.error('Socket.IO error:', err);
+            });
             
-
-            ws.on('open', function open() {
-                ws.send(JSON.stringify(req.body));
-            });
-
-            ws.on('error', function error(err) {
-                console.error('WebSocket error:', err);
-            });
+            // Desconectar el socket después de enviar los datos
+            socket.disconnect();
 
             // Lógica para enviar a la DB
             const dataRes = await createData(req.body);
+
             return dataRes;
         } else {
             throw new Error(data.error.message);
